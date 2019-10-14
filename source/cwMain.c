2b903a4c04289df54880835d500feebb0f0e12a9
Vince Richese <vrichese@mi-t6.21-school.ru>
2019-10-14 14:24:05 +0300

    In working

--- source/cwMain.c
+++ source/cwMain.c
@@ -6,7 +6,7 @@
 /*   By: vrichese <vrichese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 21:09:41 by vrichese          #+#    #+#             */
-/*   Updated: 2019/10/14 14:23:25 by vrichese         ###   ########.fr       */
+/*   Updated: 2019/10/14 14:23:59 by vrichese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int					main(int argc, char **argv)
 {
 	corewar_t	*pGameObj;
 
-	cwCreateInstanceGame			(&pGameObj);
-	pGameObj->cwKeyObjectInit		(pGameObj, argc, argv);
+	cwCreateInstanceGame		(&pGameObj);
+	pGameObj->cwKeyObjectInit	(pGameObj, argc, argv);
 	pGameObj->cwPlayerObjectInit	(pGameObj, argc, argv);
 	pGameObj->cwCarraigeObjectInit	(pGameObj);
-	pGameObj->cwArenaObjectInit		(pGameObj);
+	pGameObj->cwArenaObjectInit	(pGameObj);
 	pGameObj->cwCommandObjectInit	(pGameObj);
 	pGameObj->cwIntroducePlayers	(pGameObj);
 	pGameObj->cwArrangeUnitsOnField	(pGameObj);
