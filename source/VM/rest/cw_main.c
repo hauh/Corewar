824f6d298a7c2758c4d7b60bc7b74341d0b30c89
Vince Richese <vrichese@mi-u5.21-school.ru>
2019-10-31 16:38:36 +0300

    in working

--- source/VM/rest/cw_main.c
+++ source/VM/rest/cw_main.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cw_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbrady <dbrady@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vrichese <vrichese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 21:09:41 by vrichese          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/10/31 16:25:48 by vrichese         ###   ########.fr       */
-=======
-/*   Updated: 2019/10/31 15:27:16 by dbrady           ###   ########.fr       */
->>>>>>> 21cde07fe462407a92572315aae4f6409d13b1d1
+/*   Updated: 2019/10/31 16:38:09 by vrichese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +24,9 @@ int				main(int argc, char **argv)
 	p_game_obj->cw_arrange_units_on_field(p_game_obj);
 	p_game_obj->cw_introduce_players(p_game_obj);
 	p_game_obj->cw_start_game(p_game_obj);
-<<<<<<< HEAD
-	p_game_obj->cw_congratulations(p_game_obj);
-	ft_printf("%032b\n", p_game_obj->test);
-=======
 	cr_vis_winner(p_game_obj);
 	// p_game_obj->cw_congratulations(p_game_obj);
 	// ft_printf("%d\n", p_game_obj->p_arena_obj->cycle_amount);
->>>>>>> 21cde07fe462407a92572315aae4f6409d13b1d1
 	p_game_obj->cw_destructor(&p_game_obj);
 	return (CW_SUCCESS);
 }
