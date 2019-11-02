4e6b0a0d9d170aa93af9c4a35f4c8d8296b98e4f
smorty <hauh@ya.ru>
2019-11-02 19:08:21 +0300

    working on script

--- scripts/corewar_bruteforce.sh
+++ scripts/corewar_bruteforce.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OG_COREWAR="../vm_champs/corewar"
+OG_ASM="../vm_champs/asm"
 MY_COREWAR="../corewar"
 
 if [ -z "$1" ]; then
@@ -18,12 +19,28 @@ if [ ! -f "$OG_COREWAR" ]; then
 	exit 1
 fi
 
-for COR_FILE_1 in $( find ${1} -type f -name "*.cor" ) ; do
-	for COR_FILE_2 in $( find ${1} -type f -name "*.cor" ) ; do
-		for COR_FILE_3 in $( find ${1} -type f -name "*.cor" ) ; do
-			for COR_FILE_4 in $( find ${1} -type f -name "*.cor" ) ; do
-				echo "${COR_FILE_1} ${COR_FILE_2} ${COR_FILE_3} ${COR_FILE_4}"
-				./corewar_checker.sh ${COR_FILE_1} ${COR_FILE_2} ${COR_FILE_3} ${COR_FILE_4}
+GREEN=""
+MAGENTA="\e[35m"
+EOC="\e[0m"
+
+declare -a S_FILES_LIST=( $( find ${1} -type f -name "*.s" ) )
+
+printf "${MAGENTA}Assembling champions:\n${EOC}"
+for CHAMP in ${S_FILES_LIST[@]} ; do
+	printf "$(basename ${CHAMP} )\e[K\r"
+	${OG_ASM} ${CHAMP} &>/dev/null
+done
+printf "\e[Kdone!\n"
+
+S_FILES_LIST=( $( find ${1} -type f -name "*.cor" ) )
+
+printf "${MAGENTA}Fighting!\n${EOC}"
+for COR_FILE_1 in ${S_FILES_LIST[@]} ; do
+	for COR_FILE_2 in ${S_FILES_LIST[@]} ; do
+		for COR_FILE_3 in ${S_FILES_LIST[@]} ; do
+			for COR_FILE_4 in ${S_FILES_LIST[@]} ; do
+				printf "$(basename ${COR_FILE_1}) $(basename ${COR_FILE_2}) $(basename ${COR_FILE_3}) $(basename ${COR_FILE_4})\e[K\r"
+				./corewar_checker.sh ${COR_FILE_1} ${COR_FILE_2} ${COR_FILE_3} ${COR_FILE_4} &>/dev/null
 			done
 		done
 	done
