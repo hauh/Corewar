31e127b774bf0eeeea050f5ee572d9248f1dd098
smorty <hauh@ya.ru>
2019-11-18 23:11:42 +0300

    dirs renamed

---
+++ include/asm/asm.h
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/10 16:21:24 by smorty            #+#    #+#             */
+/*   Updated: 2019/09/26 22:05:22 by smorty           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASM_H
+# define ASM_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include "libft.h"
+# include "op.h"
+
+# define BUFF_SIZE 16
+
+# define IS_BLANK(c) (c == ' ' || c == '\t')
+# define IS_DIGIT(c) (c >= '0' && c <= '9')
+
+# define F_DISASSEMBLE 1
+# define F_OUTPUT_LOCAL 2
+
+extern int		g_cur_line;
+extern int		g_cur_col;
+
+typedef enum	e_opcode_type
+{
+	crw_undef_code,
+	crw_live,
+	crw_ld,
+	crw_st,
+	crw_add,
+	crw_sub,
+	crw_and,
+	crw_or,
+	crw_xor,
+	crw_zjmp,
+	crw_ldi,
+	crw_sti,
+	crw_fork,
+	crw_lld,
+	crw_lldi,
+	crw_lfork,
+	crw_aff
+}				t_opcode_type;
+
+typedef enum	e_param_type
+{
+	crw_undef_param,
+	crw_registry,
+	crw_direct,
+	crw_indirect
+}				t_param_type;
+
+typedef struct	s_opcode_param
+{
+	char			*link;
+	t_param_type	type;
+	int				value;
+}				t_opcode_param;
+
+typedef struct	s_label
+{
+	char			*label_name;
+	struct s_label	*next;
+}				t_label;
+
+typedef struct	s_opcode
+{
+	t_label			*labels;
+	t_opcode_param	*param[3];
+	t_opcode_type	type;
+	size_t			size;
+	unsigned char	param_code;
+	struct s_opcode	*next;
+	struct s_opcode	*prev;
+}				t_opcode;
+
+typedef struct	s_warrior
+{
+	char			*name;
+	char			*comment;
+	t_opcode		*program;
+	unsigned char	*byte_code;
+	size_t			code_size;
+}				t_warrior;
+
+char			*read_input(int fd);
+void			skip_whitespaces(char **line);
+int				ft_printf(const char *line, ...);
+void			process_argument(char *arg, int flag);
+void			error(char *err, int type);
+
+t_warrior		*parse_file(int fd);
+void			parse_title(t_warrior *warrior, int fd);
+void			parse_label(t_opcode *program, char **line);
+void			parse_opcode(t_opcode *new, char *line);
+t_opcode_param	*parse_parameter(char *line);
+
+void			calculate_sizes(t_warrior *list);
+void			assemble(t_warrior *warrior);
+void			disassemble(int fd, int fd_out);
+
+#endif
---
+++ src/asm/parse_opcode.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_opcode.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/12 23:08:37 by smorty            #+#    #+#             */
+/*   Updated: 2019/09/28 16:58:12 by smorty           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+int g_cur_col;
+
+static t_opcode_type	get_opcode_type(char **line)
+{
+	static const char	*types[17] = {NULL, "live", "ld", "st", "add", "sub",
+									"and", "or", "xor", "zjmp", "ldi", "sti",
+									"fork", "lld", "lldi", "lfork", "aff"};
+	size_t				size;
+	int					i;
+
+	i = 17;
+	while (--i)
+	{
+		size = ft_strlen(types[i]);
+		if (ft_strnequ(*line, types[i], size))
+			break ;
+	}
+	if (i == crw_undef_code)
+		error("Lexical error: invalid instruction", 1);
+	*line += size;
+	g_cur_col += size;
+	return (i);
+}
+
+static int				validate_parameters(t_opcode_type type, int code)
+{
+	if ((type == crw_live || type == crw_fork || type == crw_lfork)
+		&& code != 128)
+		return (0);
+	if ((type == crw_ld || type == crw_lld) && code != 144 && code != 208)
+		return (0);
+	if (type == crw_st && code != 80 && code != 112)
+		return (0);
+	if ((type == crw_add || type == crw_sub) && code != 84)
+		return (0);
+	if ((type == crw_and || type == crw_or || type == crw_xor)
+						&& code != 84 && code != 100 && code != 116
+						&& code != 148 && code != 164 && code != 180
+						&& code != 212 && code != 228 && code != 244)
+		return (0);
+	if ((type == crw_ldi || type == crw_lldi)
+						&& code != 84 && code != 100 && code != 148
+						&& code != 164 && code != 212 && code != 228)
+		return (0);
+	if (type == crw_sti && code != 84 && code != 100 && code != 116
+						&& code != 88 && code != 104 && code != 120)
+		return (0);
+	if (type == crw_aff && code != 64)
+		return (0);
+	return (1);
+}
+
+static void				skip_to_separator(char **line)
+{
+	while (**line && !IS_BLANK(**line) && **line != SEPARATOR_CHAR)
+	{
+		++(*line);
+		++g_cur_col;
+	}
+}
+
+void					parse_opcode(t_opcode *opcode, char *line)
+{
+	int i;
+
+	skip_whitespaces(&line);
+	if (!*line)
+		return ;
+	opcode->type = get_opcode_type(&line);
+	i = 0;
+	while (i < 3)
+	{
+		skip_whitespaces(&line);
+		if ((opcode->param[i] = parse_parameter(line)))
+		{
+			opcode->param_code |= opcode->param[i]->type << ((3 - i) * 2);
+			skip_to_separator(&line);
+			skip_whitespaces(&line);
+			if (*line && (*line++ != SEPARATOR_CHAR || i == 2))
+				error("Syntax error: wrong parameters", 1);
+		}
+		++i;
+	}
+	if (!validate_parameters(opcode->type, opcode->param_code))
+		error("Syntax error: wrong parameters types", 1);
+}
